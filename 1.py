95a3b717c34589575ac23428b56f60b82a535cfb
SashaSimonovaa <sasasimonova429@gmail.com>
2022-10-29 13:34:14 +0300

    1-5

--- 1.py
+++ 1.py
@@ -2,7 +2,9 @@ a = [i for i in range(1, 51)]
 b = [i for i in range(50, 0, -1)]
 c = [i**2 for i in range(1, 12)]
 d = [i**2 for i in range(11, 0, -1)]
+e = [i for i in range(2, 41, 2)]
 print(a)
 print(b)
 print(c)
 print(d)
+print(e)
