13bd3cbc1bf67b9c2c1abf49775c45403a30e2b7
SashaSimonovaa <sasasimonova429@gmail.com>
2022-10-29 13:53:49 +0300

    6-9

--- 3.py
+++ 3.py
@@ -1,2 +1,7 @@
 a = [i**2 if i%2!=0 else -1*i**2 for i in range(1, 11)]
+b = [i+1 if i<3 else i+j for i in range(1, 25) for j in range(i-1, i)]
+c = [i if i%2!=0 else i*3 for i in range(1, 12)]
+d = []
 print(a)
+print(b)
+print(c)
