de7be994ce7a73b70ae0af4bce5b86f80b6752e7
Александра <sasasimonova429@gmail.com>
2022-11-11 17:02:35 +0300

    6-9

--- 3.py
+++ 3.py
@@ -4,9 +4,11 @@ for i in range(1, 11):
     b.append(i+b[i-1])
 c = [i if i%2!=0 else i*3 for i in range(1, 12)]
 d = [1]
-for i in range(1, 4):
-    d.append(i+d[i-1])
-    d.append(d[i-1]*d[i])
+j = 0
+for i in range(1, 5):
+    d.append(i+d[j])
+    d.append(i*d[j+1])
+    j += 2
 print(a)
 print(b)
 print(c)
